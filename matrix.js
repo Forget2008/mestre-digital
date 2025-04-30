671b601305a45eef7fc2804c97089bacf5d92e47
Forget <143536178+Forget2008@users.noreply.github.com>
2025-04-30 12:02:24 -0300

    Add files via upload

--- matrix.js
+++ matrix.js
@@ -1,20 +1,18 @@
 const canvas = document.getElementById('matrix');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+canvas.width = window.innerWidth;
 let letters = '0101010101011011101001'.split('');
 let fontSize = 14;
-let columns = Math.floor(canvas.width / fontSize);
-let drops = Array(columns).fill(1);
-
+let columns = canvas.width / fontSize;
+let drops = Array(Math.floor(columns)).fill(1);
 function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = '#FF3D00';
   ctx.font = fontSize + 'px monospace';
   for (let i = 0; i < drops.length; i++) {
-    const text = letters[Math.floor(Math.random() * letters.length)];
+    let text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
       drops[i] = 0;
@@ -23,10 +21,3 @@ function draw() {
   }
 }
 setInterval(draw, 33);
-
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  columns = Math.floor(canvas.width / fontSize);
-  drops = Array(columns).fill(1);
-});
